5ba3c231ca415366c7bd01f9190178079ded7c13
Prince Kr. Yadav <2002princeyadavbimava@gmail.com>
2026-01-08 10:40:49 +0530

    Update frontend to use Render backend URL

--- Frontend/script.js
+++ Frontend/script.js
@@ -4,12 +4,12 @@ async function shortenURL() {
     const url = input.value;
 
     if (!url) {
-        resultDiv.innerText = "❌ Please enter a URL";
+        resultDiv.innerText = " Please enter a URL";
         return;
     }
 
     try {
-        const response = await fetch("http://127.0.0.1:8000/shorten", {
+        const response = await fetch("https://final-url-project.onrender.com/shorten", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
@@ -20,15 +20,15 @@ async function shortenURL() {
         const data = await response.json();
 
         if (response.ok) {
-            const shortUrl = `http://127.0.0.1:8000/${data.short_code}`;
+            const shortUrl = `https://final-url-project.onrender.com/${data.short_code}`;
             resultDiv.innerHTML = `
-                ✅ Short URL:<br>
+                 Short URL:<br>
                 <a href="${shortUrl}" target="_blank">${shortUrl}</a>
             `;
         } else {
             resultDiv.innerText = data.detail || "Error occurred";
         }
     } catch (error) {
-        resultDiv.innerText = "❌ Backend server not running";
+        resultDiv.innerText = " Backend server not responding";
     }
 }
